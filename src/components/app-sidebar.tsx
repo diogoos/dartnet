9865e81faf075d70932288156eb05c617d55d396
Diogo Silva <59505963+diogoos@users.noreply.github.com>
2025-07-12 10:21:46 -0300

    shadcn dashboard + clubs

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,96 @@
+"use client"
+
+import * as React from "react"
+import {
+  AudioWaveform,
+  Cog,
+  Command,
+  Globe,
+  MessageSquareText,
+  Users,
+  Car
+} from "lucide-react"
+
+import { NavSection } from "@/components/nav-section"
+import { NavUser } from "@/components/nav-user"
+import { TeamSwitcher } from "@/components/team-switcher"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+
+// This is sample data.
+const data = {
+  user: {
+    name: "Andy Kotz",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  teams: [
+    {
+      name: "DALI Lab",
+      imgLogo: "/dali.jpeg",
+      logo: null,
+    },
+    {
+      name: "Brazil Society",
+      logo: Globe,
+    },
+    {
+      name: "Formula Racing",
+      logo: Car,
+    },
+  ],
+
+  navClub: [
+    {
+      title: "Posts",
+      url: "#",
+      icon: MessageSquareText,
+      isActive: true,
+    },
+    {
+      title: "Members",
+      url: "#",
+      icon: Users,
+    },
+    {
+      title: "Settings",
+      url: "#",
+      icon: Cog,
+    },
+  ],
+
+  navPersonal: [
+    {
+      title: "Account",
+      url: "#",
+      icon: Command,
+    },
+    {
+      title: "Profile",
+      url: "#",
+      icon: AudioWaveform,
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavSection section="My Club" items={data.navClub} />
+        <NavSection section="Personal" items={data.navPersonal} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
---
+++ src/components/nav-section.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import { type LucideIcon } from "lucide-react"
+
+import {
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+export function NavSection({
+  section,
+  items,
+}: {
+  section: string,
+  items: {
+    title: string
+    url: string
+    icon?: LucideIcon
+    isActive?: boolean
+    items?: {
+      title: string
+      url: string
+    }[]
+  }[]
+}) {
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>{section}</SidebarGroupLabel>
+      <SidebarMenu>
+        {items.map((item) => (
+          <SidebarMenuItem key={item.title}>
+            <SidebarMenuButton tooltip={item.title} isActive={item.isActive}>
+              {item.icon && <item.icon />}
+              <a href={item.url}>
+                <span>{item.title}</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        ))}
+      </SidebarMenu>
+    </SidebarGroup>
+  )
+}
