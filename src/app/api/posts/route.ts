24ff45007813365f56a0ec177338e244366ceadc
Diogo Silva <59505963+diogoos@users.noreply.github.com>
2025-07-25 11:54:58 -0300

    fix bug - crashing on new post

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -85,6 +85,9 @@ export async function POST(req: NextRequest) {
           name: true,
           img: true
         }
+      },
+      likedBy: {
+        select: { id: true }
       }
     }
   })
